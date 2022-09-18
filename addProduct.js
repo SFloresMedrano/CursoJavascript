952a8defdc604370bdb7118d336926120f71104c
Simón Flores <asfloresmedrano@gmail.com>
2022-09-18 20:04:05 -0300

    Modificado para eventos

--- addProduct.js
+++ addProduct.js
@@ -1,54 +1,140 @@
-function Productos (codigo,nombre,stock,precio){
-    this.codigo = codigo;
-    this.nombre = nombre;
-    this.stock = stock;
-    this.precio = precio;
+class Productos {
+    constructor (code,name,stock,price){
+        this.code = code;
+        this.name = name;
+        this.stock = stock;
+        this.price = price;
+    };
+}
+
+function DeleteFields () {
+    const input=document.getElementsByClassName("form-control");
+    input.value="";
 };
 
+let newProduct = new Productos (0, "", 0, 0)
 
-const remover = document.getElementById("removeProduct");
-
-arrStock=[];
+let confirmation="";
+let selectedProduct =[], arrayCart=[], arrayStock=[];
 
 function AddProduct (){ 
     const addProduct = document.getElementById("addProduct");
 
-    addProduct.addEventListener("click",()=>{
+    addProduct.addEventListener("click",(e)=>{
+        e.preventDefault();
+        newProduct.code = (document.getElementById("productCode").value);
+        newProduct.name = document.getElementById("productName").value;
+        newProduct.stock = (document.getElementById("productStock").value);
+        newProduct.price = (document.getElementById("productPrice").value);        
+        
+        let div =document.createElement("div")
+        div.classList.add("card")
+        div.innerHTML=` <img src=./assets/img/${newProduct.code} alt="${newProduct.name}" class="card_image"/>
+                        <h3 class="card_h3"> ${newProduct.name}</h3>
+                        <p class="card_p"> Cantidad en Stock: ${newProduct.stock}</p>
+                        <p class="card_p"> Precio: ${newProduct.precio}</p>`;
+        addProduct.append(div)
 
 
-        let codigo = (document.getElementById("codigoProd").value);
-        let nombre = document.getElementById("nombreProd").value;
-        let stock = (document.getElementById("stockProd").value);
-        let precio = (document.getElementById("precioProd").value);
-    
-        let newProd = new Productos (codigo, nombre, stock, precio)
-    
-        let confirmacion =prompt(`Se agregara el siguiente item:
-                        ${newProd.codigo} 
-                        ${newProd.nombre}
-                        ${newProd.stock} 
-                        ${newProd.precio}
+        confirmation =prompt(`Se agregara el siguiente item:
+                        ${newProduct.code} 
+                        ${newProduct.name}
+                        ${newProduct.stock} 
+                        ${newProduct.price}
                         Confirme por SI o NO`)
 
-                        if (confirmacion.toLowerCase()==="si"){
-            arrStock.push(newProd)
 
-            alert(arrStock[0].precio)
+        if (confirmation.toLowerCase() === "si"){
+            arrayStock.push(newProduct);
+            alert ("El producto ha sido agregado como stock.");
+        
+            const input=document.getElementsByClassName("form-control");
+            input.value="";
+        
+        } else if(confirmation.toLowerCase() ==="no"){
+            alert("El producto no ha sido agregado");
+            newProduct.code = 0;
+            newProduct.name = 0;
+            newProduct.stock = 0;
+            newProduct.price = 0;
 
-        } else if(confirmacion.toLowerCase()==="no"){}       
-    
+            const input=document.getElementsByClassName("form-control");
+            input.value="";
+        }           
     });
 };
 
-function RemProduct(){
-    remProduct.addEventListener("click",()=>{
-        let codigo = parseint(document.getElementById("codigoProd").value);
+function RemoveProduct(){
+    const removeProduct = document.getElementById("removeProduct");
 
+    removeProduct.addEventListener("click",()=>{
+        let codeInput = document.getElementById("productCode").value;        
+        selectedProduct = arrayStock.find(val=> val.code === codeInput);
+        let location =arrayStock.findIndex(val=>val.code===codeInput);
 
-    })
+        if (selectedProduct != (-1)){
+
+            confirmation = prompt(`El producto a eliminar es el siguiente:
+            ${selectedProduct.code}
+            ${selectedProduct.name}
+            ${selectedProduct.stock}
+            ${selectedProduct.price}
+            Confirme por SI o NO`)
+
+                if (confirmation.toLowerCase()==="si"){
+                    alert(arrayStock[location].code + " " + arrayStock.length)
+                    arrayStock.splice(location, 1);
+                    alert (`El producto ha sido removido.`)
+                    DeleteFields();
+
+                } else if(confirmation.toLowerCase()==="no"){
+                    alert("El producto no ha sido removido.");
+                    newProduct.code = 0;
+                    newProduct.name = 0;
+                    newProduct.stock = 0;
+                    newProduct.price = 0;
+                    DeleteFields();
+                }  
+        }else{
+            alert("El producto no ha sido encontrado")
+        };
+
+        
+    });
+};
+
+function RenderCard(){
+    const showProduct=document.getElementById("showProduct");
+    const addProduct = document.getElementById("containerInput");
+    showProduct.addEventListener("click",(e)=>{
+        newProduct.code = (document.getElementById("productCode").value);
+        newProduct.name = document.getElementById("productName").value;
+        newProduct.stock = (document.getElementById("productStock").value);
+        newProduct.price = (document.getElementById("productPrice").value);  
+        
+        let div =document.createElement("div")
+        div.classList.add("card")
+        div.innerHTML=` <img src=./assets/img/${newProduct.code}.jpg alt="${newProduct.name}" class="card_image"/>
+                        <h3 class="card_h3"> ${newProduct.name}</h3>
+                        <p class="card_p"> Cantidad en Stock: ${newProduct.stock}</p>
+                        <p class="card_p"> Precio: ${newProduct.price}</p>`;
+        addProduct.append(div);
+
+})
+    ;
+};
+
+function DeleteCard(){
+    let codeInput=document.getElementById("productCode")
+    let cardCount = document.querySelectorAll("card")
+    if (cardCount.length>0){
+        codeInput.addEventListener("change",()=>{
+            alert(cardCount.length)
+            let cardItem=document.getElementById("card");   
+            cardItem.remove();
+        })
+    }
+
 }
 
-
-
-
-//Agregar event Listener para botones
+// agregar funcionalidad para que el boton no agregue de mas
--- modal-login.js
+++ modal-login.js
@@ -1,26 +1,26 @@
 const modalContainer= document.querySelector(".modal-container")
-const abrirLogin = document.getElementById("open")
-const cerrarLogin  = document.getElementById("cerrar")
+const openLogin = document.getElementById("open")
+const closeLogin  = document.getElementById("cerrar")
 const modalLogin = document.getElementById(".modal-login")
 
-let arrStock=[];
 
 
-abrirLogin.addEventListener("click",()=>{
+
+openLogin.addEventListener("click",()=>{
     modalContainer.classList.toggle("modal-active");
 });
 
-cerrarLogin.addEventListener("click",()=>{
+closeLogin.addEventListener("click",()=>{
     modalContainer.classList.remove("modal-active");
 });
 
-cerrarLogin.addEventListener("click",(e)=>{
+closeLogin.addEventListener("click",(e)=>{
     e.stopPropagation() ;
 }) ;
 
-const iniciarSesion = document.getElementById("iniciarSesion");
+const logIn = document.getElementById("logIn");
 
-iniciarSesion.addEventListener("click",()=>{
+logIn.addEventListener("click",()=>{
     let user=document.getElementById("user").value;
     let pass=document.getElementById("password").value;
 
@@ -28,47 +28,54 @@ iniciarSesion.addEventListener("click",()=>{
         alert("Bienvenido admin");
         modalContainer.classList.remove("modal-active");
         
-        let newSec=document.createElement("section");
-        newSec.classList.add(`addProduct`);
-        document.body.prepend(newSec);
-        newSec.innerHTML = `<div class="input-group mb-3">
+        let newSection=document.createElement("section");
+        newSection.classList.add(`addProduct`);
+        newSection.setAttribute("id","containerInput")
+        document.body.prepend(newSection);
+        newSection.innerHTML = `<form id="inputContainer class="inputContainer">
+                                <div class="input-group mb-3">
 
-                                <div class="form-floating">
-                                <input type="number" class="form-control" id="codigoProd" placeholder="Codigo">
-                                <label for="codigoInput">Codigo</label>
+                                    <div class="form-floating">
+                                    <input type="number" class="form-control" id="productCode" placeholder="code">
+                                    <label for="codeInput">code</label>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="input-group mb-3">
+                                <div class="input-group mb-3">
 
-                                <div class="form-floating">
-                                <input type="text" class="form-control" id="nombreProd" placeholder="Nombre">
-                                <label for="nombreProd">Nombre del Producto</label>
+                                    <div class="form-floating">
+                                    <input type="text" class="form-control" id="productName" placeholder="name">
+                                    <label for="productName">name del Producto</label>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="input-group mb-3">
+                                <div class="input-group mb-3">
 
-                                <div class="form-floating">
-                                <input type="number" class="form-control" id="stockProd" placeholder="Stock">
-                                <label for="nombreProd">Stock del Producto</label>
+                                    <div class="form-floating">
+                                    <input type="number" class="form-control" id="productStock" placeholder="Stock">
+                                    <label for="productName">Stock del Producto</label>
+                                    </div>
                                 </div>
-                            </div>
-                            <div class="input-group mb-3">
+                                <div class="input-group mb-3">
 
-                                <div class="form-floating">
-                                <input type="number" class="form-control" id="precioProd" placeholder="Precio">
-                                <label for="precioProd">Precio del Producto</label>
+                                    <div class="form-floating">
+                                    <input type="number" class="form-control" id="productPrice" placeholder="price">
+                                    <label for="productPrice">price del Producto</label>
+                                    </div>
                                 </div>
-                            </div>
-        
-                            <input class="btn button btnadd" type="submit" id="addProduct" value="Agregar Producto">
-                            <input class="btn button btnrem" type="reset" id="removeProduct" value="Remover Producto"> `;
-
+                                
+                                
+                                <input class="btn button btnadd" type="submit" id="addProduct" value="Agregar Producto">
+                                <input class="btn button btnrem" type="reset" id="removeProduct" value="Remover Producto">
+                                <input class="btn button btnshow" type="button" id="showProduct" value="Mostrar Producto">
+                                </form>`
 
 
                             AddProduct();
+                            RemoveProduct();
+                            RenderCard();
+                            DeleteCard();
 
 
-    }else if(user.toLowerCase()==="user" && pass.toLowerCase()==="user"){
+    }else if(user.toLowerCase() ==="user" && pass.toLowerCase() ==="user"){
         alert("Bienvenido user");
         modalContainer.classList.remove("modal-active");
     }else {
